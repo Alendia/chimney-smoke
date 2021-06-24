456e01a0dab0c1f8773332c98d68168cdfadfcf9
Alendia <alendia0807@gmail.com>
2021-06-24 10:18:29 +0800

    Initialize project using Create React App

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import Personal from './pages/personal/personal';
+
+function App() {
+  return (
+    <div className="App">
+      <Personal />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/pages/personal/personal.js
@@ -0,0 +1,82 @@
+import React from "react";
+import Navigation from "../../components/navigation/navigation";
+import "./style.css";
+
+import RightArrow from "../../static/img/rightArrow.svg";
+import AnalysisPic from "../../static/img/analysis.svg";
+import TicketsPic from "../../static/img/tickets.svg";
+
+function Info() {
+  return (
+    <div className="profile">
+      <div className="profile-pic"></div>
+      <p className="profile-name">猫猫</p>
+    </div>
+  );
+}
+
+function Feature() {
+  return (
+    <div className="feature-block">
+      <div>
+        <a href="" className="feature-link">
+          <img className="feature-img" src={AnalysisPic} alt="" />
+          <p>个性分析</p>
+        </a>
+      </div>
+      <div className="divider"></div>
+      <div>
+        <a href="" className="feature-link">
+          <img className="feature-img" src={TicketsPic} alt="" />
+          <p>优惠券</p>
+        </a>
+      </div>
+    </div>
+  );
+}
+
+function Settings() {
+  return (
+    <div className="settings-block">
+      <div className="settings-link">
+        <a href="">
+          <p>口味设置</p>
+          <img src={RightArrow} alt="" />
+        </a>
+      </div>
+      <div className="settings-divider"></div>
+      <div className="settings-link">
+        <a href="">
+          <p>积分兑换</p>
+          <img src={RightArrow} alt="" />
+        </a>
+      </div>
+      <div className="settings-divider"></div>
+      <div className="settings-link">
+        <a href="">
+          <p>联系客服</p>
+          <img src={RightArrow} alt="" />
+        </a>
+      </div>
+      <div className="settings-divider"></div>
+      <div className="settings-link">
+        <a href="">
+          <p>更多</p>
+          <img src={RightArrow} alt="" />
+        </a>
+      </div>
+    </div>
+  );
+}
+
+export default function Personal() {
+  return (
+    <div className="personal">
+      <div className="personal-bg"></div>
+      <Info />
+      <Feature />
+      <Settings />
+      <Navigation className="navigation" />
+    </div>
+  );
+}
